3c6b8d50fe1553f0e62690c7b914b1581a04084f
costagavras <cgavras@yahoo.ca>
2018-10-15 16:11:26 -0400

    pledge by non-owner validation

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,13 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+
+  validate :owner_of_project
+
+  def owner_of_project
+    if self.project.user == self.user
+      errors.add(:notice, "You're the owner of the project!!!)")
+    end
+  end
+
 end
