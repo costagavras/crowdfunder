c015377fb3be4f9484b37bd6642d4903d63dd725
StephenVarela <varelast92@gmail.com>
2018-10-15 16:02:20 -0400

    adding pledge validation

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,13 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :back_own_project
+end
+
+def back_own_project
+    if self.user == self.project.user
+      errors.add(:Owner, 'should not be able to pledge towards own project')
+    else
+
+    end
 end
