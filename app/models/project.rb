aaea1be18962cde333daa25e82d074f551d80efb
StephenVarela <varelast92@gmail.com>
2018-10-15 15:43:11 -0400

    added validation to ensure project has an owner before saving

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,15 @@ class Project < ActiveRecord::Base
   belongs_to :user # project owner
 
   validates :title, :description, :goal, :start_date, :end_date, presence: true
+  validate :has_owner
 end
+
+private
+
+  def has_owner
+      if self.user_id
+
+      else
+        errors.add(:Project, 'should not save without owner.')
+      end
+  end
